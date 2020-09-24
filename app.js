67d7a1e81a6302b8bed442ef3ba360c33eb1f554
ChazzKreutzkamp <chazz.kreutzkamp@gmail.com>
2020-09-23 22:02:22 -0600

    The code now generates an html file with basic html, and has been modularized.

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
 
-const printProfileData = (profileDataArr) => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
-    console.log('=================');
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-}
+const [name, github] = profileDataArgs;
 
-printProfileData(profileDataArgs);
+// equivalent is listed below
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
+
+
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!')
+});
+//const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
+//console.log(generatePage(name, github));
+
+
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
+
+// const printProfileData = (profileDataArr) => {
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//         console.log(profileDataArr[i]);
+//     }
+//     console.log('=================');
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// }
+
+// printProfileData(profileDataArgs);
